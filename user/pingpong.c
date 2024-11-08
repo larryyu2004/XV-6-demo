8e9d9b8f681d93289ce6aba4b91c8fb3dc7de77d
larryyu2004 <jiayiyu@myyahoo.com>
2024-11-08 14:47:27 +1030

    Lab1, pingpong improve

--- user/pingpong.c
+++ user/pingpong.c
@@ -57,7 +57,7 @@ int main(){
             printf("read from pipe failed");
             exit(1);
         }
-        printf("%d: received: pong$", getpid());
+        printf("%d: received: %s", getpid(), buf);
 
         //fd[0] and fd[1] in the parent process are no longer used, close them
         close(fd[0]);
@@ -77,7 +77,7 @@ int main(){
             printf("child read from pipe failed");
             exit(1);
         };
-        printf("%d: received: ping$", getpid());
+        printf("%d: received: %s", getpid(), buf);
 
         //Child writes the message into pipe
         if(write(fd[1], "pong", strlen("pong")+1) == -1){
